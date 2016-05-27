23c99e3cbe6d0c0767f865e7f6eefd99ec13982c
yshiga <yuichi.shiga@gmail.com>
2016-05-28 06:42:14 +0900

    refactor and format

--- qa-user-profile-layer.php
+++ qa-user-profile-layer.php
@@ -1,57 +1,68 @@
 <?php
-class qa_html_theme_layer extends qa_html_theme_base {
 
-	public function post_avatar_meta($post, $class, $avatarprefix=null, $metaprefix=null, $metaseparator='<br/>') {
-		qa_html_theme_base::post_avatar_meta($post, $class, $avatarprefix, $metaprefix, $metaseparator);
+class qa_html_theme_layer extends qa_html_theme_base
+{
+    public function post_avatar_meta($post, $class, $avatarprefix = null, $metaprefix = null, $metaseparator = '<br/>')
+    {
+        qa_html_theme_base::post_avatar_meta($post, $class, $avatarprefix, $metaprefix, $metaseparator);
 
-		$userId = $post['raw']['userid'];
-		if(!isset($userId) || $class !== 'qa-q-view') {
-			return;	
-		}
+        $userId = $post['raw']['userid'];
+        if (!isset($userId) || $class !== 'qa-q-view') {
+            return;
+        }
 
-		$profileItems = $this->getUserprofile($userId);
-		$this->displayUserprofile($profileItems, $userId);
-	}
+        $profileItems = $this->getUserprofile($userId);
+        $this->displayUserprofile($profileItems, $userId);
+    }
 
-	private function displayUserprofile($profileItems,$userId){
+    private function displayUserprofile($profileItems, $userId)
+    {
+        $this->output('<p>質問者の基本情報</p>');
 
-		echo '<p>質問者の基本情報</p>';
+        if (!count($profileItems)) {
+            $message = '';
+            $logginUserId = qa_get_logged_in_userid();
+            if ($userId == $logginUserId) {
+                $message .= 'プロフィールを入力してください';
+                $message .= '<a href="'.qa_path('account').'">'.こちら.'</a>';
+            } else {
+                $message .= 'この質問者はプロフィールを入力していません';
+            }
+            $this->output($message);
 
-		if(!count($profileItems)) {
-			$logginUserId = qa_get_logged_in_userid();
-			if($userId == $logginUserId){
-				echo 'プロフィールを入力してください';
-				echo '<a href="'.qa_path('account').'">' .こちら . '</a>';
-			}else{
-				echo 'この質問者はプロフィールを入力していません';
-			}
-			return;
-		}
-		foreach($profileItems as $item)  {
-			echo '<p>・';
-			echo $item['title'] . ' : ';
-			if($item['content']){
-				$urlPattern = '/^http.+/';
-				if(preg_match($urlPattern, $item['content']) == 0){
-					echo $item['content'];
-				} else {
-					echo '<a href="' . $item['content'] . '" >' . $item['content']  . '</a>';
-				}
-			} else {
-				echo '未記入';
-			}
-			echo '</p>';
-		}
-	}
+        } else {
+            $message = '';
+            foreach ($profileItems as $item) {
+								$message .= '<p>・';
+								$message .= $item['title'].' : ';
+                if ($item['content']) {
+                    $urlPattern = '/^http.+/';
+                    if (preg_match($urlPattern, $item['content']) == 0) {
+												$message .= $item['content'];
+                    } else {
+												$message .= '<a href="'.$item['content'].'" >'.$item['content'].'</a>';
+                    }
+                } else {
+										$message .= '未記入';
+                }
+								$message .= '</p>';
+            }
+            $this->output($message);
+        }
+    }
 
-	private function getUserprofile($userId){
-		$sql = $this->createUserprofileSQL($userId, array_keys($fields));
-		$result = qa_db_query_sub($sql); 
-		return qa_db_read_all_assoc($result);
-	}
+    private function getUserprofile($userId)
+    {
+        $sql = $this->createUserprofileSQL($userId, array_keys($fields));
+        $result = qa_db_query_sub($sql);
 
-	private function createUserprofileSQL($userId){
-		$sql = "select fields.content AS title, profile.content from qa_userprofile as profile inner join qa_userfields as fields on profile.title = fields.title where profile.userid = " . $userId;
-		return $sql;
-	}
+        return qa_db_read_all_assoc($result);
+    }
+
+    private function createUserprofileSQL($userId)
+    {
+        $sql = 'select fields.content AS title, profile.content from qa_userprofile as profile inner join qa_userfields as fields on profile.title = fields.title where profile.userid = '.$userId;
+
+        return $sql;
+    }
 }
