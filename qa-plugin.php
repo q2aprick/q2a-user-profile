710fac373decf937278641f00ae549bebdc61e08
yaimapp <gmonday730-mbp@yahoo.co.jp>
2016-07-07 15:17:23 +0900

    Add Admin option

--- qa-plugin.php
+++ qa-plugin.php
@@ -2,7 +2,7 @@
 
 /*
 	Plugin Name: User profile in question
-	Plugin URI: 
+	Plugin URI:
 	Plugin Description: Add user profile in question page
 	Plugin Version: 0.1
 	Plugin Date: 2015-06-06
@@ -10,7 +10,7 @@
 	Plugin Author URI:https://github.com/yshiga
 	Plugin License: GPLv2
 	Plugin Minimum Question2Answer Version: 1.7
-	Plugin Update Check URI: 
+	Plugin Update Check URI:
 */
 
 if (!defined('QA_VERSION')) { // don't allow this page to be requested directly from browser
@@ -18,4 +18,7 @@ if (!defined('QA_VERSION')) { // don't allow this page to be requested directly 
 	exit;
 }
 
-qa_register_plugin_layer('qa-user-profile-layer.php', 'user profile');	
+// layer
+qa_register_plugin_layer('qa-user-profile-layer.php', 'user profile');
+// admin
+qa_register_plugin_module('module', 'qa-user-profile-admin.php', 'qa_user_profile_admin', 'User Profile Admin');
---
+++ qa-user-profile-admin.php
@@ -0,0 +1,47 @@
+<?php
+
+class qa_user_profile_admin {
+
+	function option_default($option) {
+		switch($option) {
+			case 'qa_user_profile_max_length':
+				return 0;
+			default:
+				return null;
+		}
+	}
+
+	function allow_template($template) {
+		return ($template != 'admin');
+	}
+
+	function admin_form(&$qa_content){
+		// process the admin form if admin hit Save-Changes-button
+		$ok = null;
+		if (qa_clicked('qa_user_profile_save')) {
+			qa_opt('qa_user_profile_length', qa_post_text('qa_user_profile_length'));
+			$ok = qa_lang('admin/options_saved');
+		}
+
+		// form fields to display frontend for admin
+		$fields = array();
+
+		$fields[] = array(
+			'label' => 'Max Length: ',
+			'type' => 'number',
+			'value' => (int)qa_opt('qa_user_profile_length'),
+			'tags' => 'name="qa_user_profile_length"',
+		);
+
+		return array(
+			'ok' => ($ok && !isset($error)) ? $ok : null,
+			'fields' => $fields,
+			'buttons' => array(
+				array(
+					'label' => qa_lang_html('main/save_button'),
+					'tags' => 'name="qa_user_profile_save"',
+				),
+			),
+		);
+	}
+}
