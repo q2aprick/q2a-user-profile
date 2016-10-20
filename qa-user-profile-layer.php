22e12d949b2d49e93e4e9d2676929876480643ef
saniman <saniman32@gmail.com>
2016-10-20 15:42:56 +0900

    プロフォールのスタイル修正

--- qa-user-profile-layer.php
+++ qa-user-profile-layer.php
@@ -3,11 +3,15 @@
 class qa_html_theme_layer extends qa_html_theme_base
 {
 		// show information about user
-	public function post_avatar_meta($post, $class, $profile_show = false,$avatarprefix = null, $metaprefix = null, $metaseparator = '<br/>')
+	public function post_avatar_meta($post, $class,$post_meta_show = true, $avatarprefix = null, $metaprefix = null, $metaseparator = '<br/>')
 	{
 		
-		qa_html_theme_base::post_avatar_meta($post, $class, $avatarprefix, $metaprefix, $metaseparator);
-
+		$this->output('<span class="'.$class.'-avatar-meta">');
+		$this->avatar($post, $class, $avatarprefix);
+		if($post_meta_show)
+			$this->post_meta($post, $class, $metaprefix, $metaseparator);
+		$this->output('</span>');
+		
 		$userId = $post['raw']['userid'];
 		$handle = $post['raw']['handle'];
 		if (!isset($userId)) {
@@ -18,11 +22,11 @@ class qa_html_theme_layer extends qa_html_theme_base
 		}
 
 		$profileItems = $this->getUserprofile($userId);
-		if($profile_show)
-			$this->displayUserprofile($profileItems, $userId, $handle);
+		
+		$this->displayUserprofile($post,$profileItems, $userId, $handle);
 	}
 
-	private function displayUserprofile($profileItems, $userId, $handle = null)
+	private function displayUserprofile($post,$profileItems, $userId, $handle = null)
 	{
 		if (!count($profileItems)) {
 			$message = '';
@@ -39,44 +43,62 @@ class qa_html_theme_layer extends qa_html_theme_base
 			$message = '';
 			$profile_max_length = qa_opt('qa_user_profile_max_length');
 			$read_more = $this->get_read_more($handle);
-			$this->output('<ul class="mdl-list">');
+
+			$this->output('<div class="profile">');
+			$this->output('<div class="mdl-typography--subhead">');
+			$this->post_meta_who($post, 'meta');
+			$this->output('<button type="button" id="user-'.$userId.'" class="mdl-button mdl-js-button mdl-button--icon"><i class="material-icons">more</i></button>');
+			$this->output('</div>');
+			
+			$profile_intro = $profileItems[2]['content'];
+			$length = mb_strlen($profile_intro, 'UTF-8');
+			$this->output('<div class="profile_intro">');
+			//最大文字数になったらもっと読むリンク追加
+			$profile_intro_content = $length > $profile_max_length ? $this->output(mb_substr($profile_intro,0,$profile_max_length - 6, 'UTF-8') . $read_more) : $this->output($profile_intro) ;
+			
+			$this->output($profile_intro_content);
+			$this->output('</div>');
+
+			$this->output('</div>');
+			$this->output('<ul class="mdl-menu mdl-js-menu mdl-js-ripple-effect" for="user-'.$userId.'">');
 			foreach ($profileItems as $item) {
-				//開始タグと閉じタグを指定　未記入ならその項目は表示しない
-				$wrap_list_tag = $item['content'] === ''?'':'<li class="mdl-list__item">';
-				$close_list_tag = $item['content'] === ''?'':'</li>';
-				$this->output($wrap_list_tag);
-				//開始タグと閉じタグを指定　未記入ならその項目は表示しない
-				$wrap_title_tag = $item['content'] === ''?'':'<span class="mdl-list__item-primary-content">';
-				$close_title_tag = $item['content'] === ''?'':'</span>';
-				$item_title = $item['content'] === ''?'':$item['title'];
-				$this->output($wrap_title_tag);
-				$this->output($item_title);
-				$this->output($wrap_title_tag);
-
-				//開始タグと閉じタグを指定　未記入ならその項目は表示しない
-				$wrap_content_tag = $item['content'] === ''?'':'<span  class="mdl-list__item-text-body">';
-				$close_content_tag = $item['content'] === ''?'':'</span>';
-				$this->output($wrap_content_tag);
-
 				if ($item['content']) {
-					if ($item['title'] === '自己紹介') {
-						$length = mb_strlen($item['content'], 'UTF-8');
-						if ($length > $profile_max_length) {
-							$this->output(mb_substr($item['content'],0,$profile_max_length - 6, 'UTF-8') . $read_more);
+					
+					$this->output('<li>');
+
+					//自己紹介は項目名非表示
+					$item_title = $item['title'] === '自己紹介'?'':$item['title'];
+					$this->output('<span class="mdl-typography--body-2">');
+					$this->output($item_title);
+					$this->output('</span>');
+					
+
+					//開始タグと閉じタグを指定　未記入ならその項目は表示しない
+					$wrap_content_tag = $item['content'] === ''?'':'<span  class="mdl-typography--body-1-color-contrast">';
+					$close_content_tag = $item['content'] === ''?'':'</span>';
+					$this->output($wrap_content_tag);
+
+					if ($item['content']) {
+						if ($item['title'] === '自己紹介') {
+							$length = mb_strlen($item['content'], 'UTF-8');
+							if ($length > $profile_max_length) {
+								$this->output(mb_substr($item['content'],0,$profile_max_length - 6, 'UTF-8') . $read_more);
+							} else {
+								$this->output($item['content']);
+							}
 						} else {
-							$this->output($item['content']);
+							$urlPattern = '/^http.+/';
+							if (preg_match($urlPattern, $item['content']) == 0) {
+								$this->output($item['content']);
+							} else {
+								$this->output('<a class="mdl-button mdl-js-button mdl-button--icon" href="'.$item['content'].'" ><i class="material-icons">open_in_new</i></a>');
+							}
 						}
-					} else {
-						$urlPattern = '/^http.+/';
-						if (preg_match($urlPattern, $item['content']) == 0) {
-							$this->output($item['content']);
-						} else {
-							$this->output('<a href="'.$item['content'].'" >'.$item['content'].'</a>');
-						}
-					}
-				} 
-				$this->output($close_content_tag);
-				$this->output($close_list_tag);
+						
+					} 
+					$this->output($close_content_tag);
+					$this->output('</li>');
+				}
 			}
 			$this->output('</ul>');
 		}
